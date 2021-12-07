fae9b35e60662e6affac76047aaaf143391e6699
Erick7451 <erick7451@gmail.com>
2021-12-07 15:43:57 -0600

    modified vid2frames module to be able to handle subset of videos

--- cmd/vid2frames.py
+++ cmd/vid2frames.py
@@ -17,7 +17,19 @@ map = pd.read_csv(args.meta)
 vfiles = map.iloc[:, 0].values.reshape(-1).tolist() # name of video file
 mdirs = map.iloc[:, 1].values.reshape(-1).tolist() # name of corresponding mot sequence directory
 
+# extract all video directories in `mpath`
+live_mdirs: list = os.listdir(args.mpath)
+"""
+It may be the case that there exists more videos in `meta` then there are in `mpath`.
+For example, we may have directories for only three videos in `mpath` out of the
+20 videos listed in `meta`.
+
+Thus, `live_mdirs` will allows us to only extract subset of the videos in `mpath`
+"""
+
 for vfile, mdir in zip(vfiles, mdirs):
+    if mdir not in live_mdirs:
+        continue 
     source = os.path.join(args.vpath, vfile)
     dest = os.path.join(args.mpath, mdir, "img1")
     ffmpegexe = f"ffmpeg -i \"{source}\" -start_number 1 -b:v 10000k -vsync 0 -an -y -q:v 16 \"{dest}/%06d.jpg\" &"
